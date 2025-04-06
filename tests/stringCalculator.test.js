5adfe75d280ee984208b65e19b213ee4dc1eb2c6
Sahilsj <25beastsj@gmail.com>
2025-04-06 17:55:00 +0530

    Add test for a single number  input

--- tests/stringCalculator.test.js
+++ tests/stringCalculator.test.js
@@ -3,3 +3,7 @@ const {add} = require('../src/stringCalculator');
 test('returns 0 for an empty string', () => {
     expect(add("")).toBe(0);
 });
+
+test('returns number fro single value', () => {
+    expect(add("1")).toBe(1);
+});
