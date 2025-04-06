c3e370da9765143eebb6aaa6827249170fc2c5bd
Sahilsj <25beastsj@gmail.com>
2025-04-06 21:18:36 +0530

    Implement negative number handling

--- src/stringCalculator.js
+++ src/stringCalculator.js
@@ -1,12 +1,17 @@
 function add(numbers) {
    if (numbers === "") return 0;
-     let delimiter = /,|\n/; // default delimiters: comma and newline
+     let delimiter = /,|\n/;
      if (numbers.startsWith("//")) {
        const parts = numbers.split("\n");
        delimiter = new RegExp(parts[0].substring(2));
        numbers = parts.slice(1).join("\n");
      }
-     return numbers.split(delimiter).reduce((sum, num) => sum + Number(num), 0);
+     const numberArray = numbers.split(delimiter).map(Number);
+     const negatives = numberArray.filter(n => n < 0);
+     if (negatives.length) {
+       throw new Error("negative numbers not allowed " + negatives.join(","));
+       }
+      return numberArray.reduce((sum, num) => sum + num, 0);
 }
 
 module.exports = { add };
