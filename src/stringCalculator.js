aebeb4a894b1a6dfa69a01a566e975f078898f6c
Sahilsj <25beastsj@gmail.com>
2025-04-06 21:05:53 +0530

    Implement support for new line delimiters

--- src/stringCalculator.js
+++ src/stringCalculator.js
@@ -1,6 +1,7 @@
 function add(numbers) {
     if (numbers === "") return 0;
-    return numbers.split(",").reduce((sum, num) => sum + Number(num),0);
+      const delimitersNormalized = numbers.replace(/\n/g, ",");
+    return delimitersNormalized.split(",").reduce((sum, num) => sum + Number(num), 0);
 }
 
 module.exports = { add };
